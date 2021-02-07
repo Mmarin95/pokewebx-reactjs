dbae694275f9d5a17db7529494a9dc2d8ced760e
Miquel Marin <mmarin_79@hotmail.com>
2021-02-07 18:10:35 +0100

    search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+// Do I need the useState or it is enough with localStorage/variable?
 export default function SearchBar({ BASE_URL, totalPokemons }) {
   const [pokemonsNameList, setPokemonsNameList] = useState();
-
+  const URL = BASE_URL + `?limit=${totalPokemons}`;
   useEffect(() => {
-    // const pokeNames = localStorage.getItem("pokemonsNameList");
-    // if (pokeNames) {
-    //   setPokemonsNameList(pokeNames.split(","));
-    // } else {
-      axios.get(BASE_URL + `?limit=${totalPokemons}`).then((res) => {
+    const pokeNames = localStorage.getItem("pokemonsNameList");
+    if (pokeNames) {
+      setPokemonsNameList(pokeNames.split(","));
+    } else {
+      axios.get(URL).then((res) => {
         const pokeNames = res.data.results.map((result) => result.name);
         setPokemonsNameList(pokeNames);
         localStorage.setItem("pokemonsNameList", pokeNames);
-       });
-    // }
+      });
+    }
   }, [totalPokemons]);
 
   return <div></div>;
